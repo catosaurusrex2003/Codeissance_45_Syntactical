d973687c3b70e877fcf8d9818a5b65c03ac077c9
Neil Shukla <neilpshukla221203@gmail.com>
2022-09-23 23:35:21 +0530

    neil did chatbot

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./AccountLogin/LoginPage.jsx"
 import Navbar from "./Navbar/Navbar.jsx"
-// import MainLayout from "./HomePage/MainLayout.jsx";
 import HomeMaster from "./HomePage/HomeMaster.jsx";
-
+import ChatLayout from "./Forum/ChatLayout.jsx";
+import RightParticipantsPanel from "./Forum/RightParticipantsPanel.jsx";
+ 
 export default function App() {
   return (
     <div>
@@ -15,7 +16,7 @@ export default function App() {
           <Route path="/home" element={[<Navbar/>,<HomeMaster/>]} />
           {/* <Route path="/home" element={<HomeMaster />} /> */}
           {/* <Route path="/home" element={[<Navbar/>,<MainLayout/>]} /> */}
-          {/* <Route path="/home" element={<Contact />} /> */}
+          <Route path="/chat" element={[<Navbar/>, <ChatLayout/>, <RightParticipantsPanel/>]} />
           {/* <Route path="*" element={<NoPage />} /> */}
       </Routes>
     </BrowserRouter>
